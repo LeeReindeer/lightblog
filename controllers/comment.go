1b2f898e45d0fa0129bfabc2d0febaf156a2d46d
LeeReindeer <reindeerlee.work@gmail.com>
2018-07-07 21:11:38 +0800

    :sparkles: Feat: Comment

---
+++ controllers/comment.go
@@ -0,0 +1,70 @@
+package controllers
+
+import (
+	"github.com/LeeReindeer/lightblog/models"
+	"github.com/LeeReindeer/lightblog/util"
+	"github.com/astaxie/beego"
+	"log"
+	"strconv"
+	"time"
+)
+
+type CommentController struct {
+	beego.Controller
+}
+
+//todo GET: //comment?comm_id=<id>&redirect=<pre url>
+func (this *CommentController) DeleteComment() {
+
+}
+
+// POST: /comment
+func (this *CommentController) NewComment() {
+	blogId, err := this.GetInt64("blogId")
+	if err != nil {
+		log.Println(err.Error())
+		this.Redirect("/", 302)
+		return
+	}
+	fromUserId, err := this.GetInt64("fromUserId")
+	log.Println("comment from user:", fromUserId)
+	if err != nil {
+		log.Println(err.Error())
+		this.Redirect("/", 302)
+		return
+	}
+
+	var fromUser models.User
+	var toUser models.User
+	var toUserId = 0
+	var comment *models.Comment
+	content := this.GetString("commentContent")
+	toUserIdStr := this.GetString("toUserId")
+
+	fromUser = *models.GetUserById(fromUserId)
+	if len(toUserIdStr) != 0 {
+		//reply to comment
+		toUserId, _ = strconv.Atoi(toUserIdStr)
+		log.Println("reply to user:", toUserId)
+		toUser = *models.GetUserById(int64(toUserId))
+	}
+
+	comment = &models.Comment{CommBlogId: blogId, CommFromUid: fromUserId, CommToUid: int64(toUserId), CommContent: content, CommTime: time.Now(),
+		CommFromName: fromUser.UserName, CommFromAvatar: fromUser.UserAvatar, CommToName: toUser.UserName, CommToAvatar: toUser.UserAvatar}
+	models.SaveComment(comment)
+	models.IncBlogComment(blogId)
+
+	this.Redirect(this.GetString("redirect"), 302)
+}
+
+// GET" /comment/like?comm_id=<id>
+func (this *CommentController) LikeComment() {
+	redirect := this.GetString("redirect")
+	commId, ok := util.StringToInt64(this.GetString("comm_id"))
+	if !ok {
+		this.Redirect(redirect, 302)
+		return
+	}
+	models.LikeComment(commId)
+	this.Redirect(redirect, 302)
+}
---
+++ models/comment_model.go
@@ -0,0 +1,43 @@
+package models
+
+import (
+	"github.com/LeeReindeer/lightblog/util"
+	"github.com/astaxie/beego/orm"
+	"log"
+)
+
+func GetAllComments(blogId int64) (comments []Comment) {
+	o := orm.NewOrm()
+	_, err := o.QueryTable("comment").Filter("comm_blog_id", blogId).All(&comments)
+	if err != nil {
+		log.Println(err.Error())
+		return nil
+	}
+	return
+}
+
+func SaveComment(comment *Comment) (commId int64) {
+	o := orm.NewOrm()
+	commId, err := o.Insert(comment)
+	util.CheckDBErr(err)
+	log.Println("new comment id: ", commId)
+	return
+}
+
+func DeleteComment(comment *Comment) (commId int64) {
+	o := orm.NewOrm()
+	commId, err := o.Delete(comment)
+	util.CheckDBErr(err)
+	log.Println("new comment id: ", commId)
+	return
+}
+
+func LikeComment(commId int64) {
+	if commId == 0 {
+		log.Println("error: comm_id = 0")
+		return
+	}
+	db, err := orm.GetDB()
+	_, err = db.Exec("UPDATE comment SET comm_like=comm_like+1 WHERE comm_id=?", commId)
+	util.CheckDBErr(err)
+}
