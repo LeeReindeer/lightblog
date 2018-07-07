2639decb0dc8efed4dccc2678e754ffb56e9293f
LeeReindeer <reindeerlee.work@gmail.com>
2018-07-07 20:57:10 +0800

    :ok_hand: Add some util func

--- util/util.go
+++ util/util.go
@@ -1,6 +1,10 @@
 package util
 
-import "log"
+import (
+	"github.com/astaxie/beego/context"
+	"log"
+	"strconv"
+)
 
 func CheckDBErr(err error) {
 	if err != nil {
@@ -19,4 +23,32 @@ func CheckAndFatal(err error) {
 		panic(err)
 		log.Fatalln("exit(1)")
 	}
+}
+
+func ClearCookies(ctx *context.Context) {
+	ctx.SetCookie("uid", "")
+	ctx.SetCookie("username", "")
+	ctx.SetCookie("login", "false")
+	ctx.SetCookie("p", "")
+}
+
+func StringToInt64(str string) (int64, bool) {
+	i, err := strconv.Atoi(str)
+	if err != nil {
+		log.Println(err.Error())
+		return 0, false
+	}
+	return int64(i), true
+}
+
+func GetUserIdFromCookie(ctx *context.Context) (int64, bool) {
+	uidStr := ctx.GetCookie("uid")
+	if len(uidStr) == 0 {
+		return 0, false
+	}
+	uid, ok := StringToInt64(uidStr)
+	if !ok {
+		return 0, false
+	}
+	return uid, true
 }
