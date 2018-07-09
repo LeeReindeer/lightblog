e2441a8a3e9a18534d3ee72d5deaae8846b864ed
LeeReindeer <reindeerlee.work@gmail.com>
2018-07-09 13:37:53 +0800

    :sparkles: New feat: tagged blogs page

---
+++ controllers/tag.go
@@ -0,0 +1,43 @@
+package controllers
+
+import (
+	"github.com/LeeReindeer/lightblog/models"
+	"github.com/LeeReindeer/lightblog/util"
+	"github.com/astaxie/beego"
+)
+
+const TagPrefix string = "#"
+
+type TagController struct {
+	beego.Controller
+}
+
+// GET:/tag/
+func (this *TagController) ShowAllTags() {
+
+}
+
+// GET:/tag/<tag_id>/
+func (this *TagController) ShowBlogWithTag() {
+	uid, ok := util.GetUserIdFromCookie(this.Ctx)
+	if !ok {
+		Logout(this.Ctx)
+		return
+	}
+	tagId, ok := util.GetIdFromUrl(this.Ctx, "tag_id")
+	if !ok {
+		this.Redirect("/", 302)
+		return
+	}
+	tag := models.GetTagById(tagId)
+	blogs := models.GetBlogsWithTag(tagId)
+
+	//modify title
+	this.Data["title"] = TagPrefix + tag.TagName
+
+	this.Data["tag"] = tag
+	this.Data["thisUser"] = models.GetUserById(uid)
+	this.Data["blogs"] = blogs
+	this.Layout = "layout.html"
+	this.TplName = "tag_blog.html"
+}
--- models/tag_model.go
+++ models/tag_model.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"github.com/LeeReindeer/lightblog/util"
 	"github.com/astaxie/beego/orm"
 	"log"
 )
@@ -15,6 +16,36 @@ func GetTagById(tagId int64) *Tag {
 	tag := Tag{TagId: tagId}
 	o.Read(&tag)
 	return &tag
+}
+
+func GetBlogsWithTag(tagId int64) (blogs []LightBlog) {
+	if tagId == 0 {
+		return nil
+	}
+
+	o := orm.NewOrm()
+	_, err := o.Raw("SELECT * FROM blog WHERE blog_tag_id=? ORDER BY blog_time DESC", tagId).QueryRows(&blogs)
+	if err != nil {
+		return nil
+	}
+
+	userMap := make(map[int64]*User)
+	for i, blog := range blogs {
+		if userMap[blog.BlogUid] == nil {
+			user := User{UserId: blog.BlogUid}
+			// only read a user once
+			err = o.Read(&user)
+			util.CheckDBErr(err)
+			userMap[blog.BlogUid] = &user
+		}
+
+		blogs[i].Tag = *GetTagById(tagId)
+		blogs[i].BlogPreview = getBlogPreview(blog.BlogContent)
+		blogs[i].BlogTimeString = blog.BlogTime.Format("2006-01-02 15:04:05")
+		blogs[i].BlogUsername = userMap[blog.BlogUid].UserName
+		blogs[i].BlogUserAvatar = userMap[blog.BlogUid].UserAvatar
+	}
+	return
 }
 
 // Insert a new tag, if no err, return TagId, true
